4b50c0445e4f640030e4d18c498aa5aacb16c330
HyungGi Kim <sokoban@naver.com>
2024-07-14 13:52:50 +0900

    second

---
+++ src/main/java/com/restapi/restdemo/StartClass.java
@@ -0,0 +1,13 @@
+package com.restapi.restdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StartClass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartClass.class, args);
+    }
+}
